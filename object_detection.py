a20b122739bf6a6148f8d2d00c1b8e496f25cc02
Gwent <gwentkrause@gmail.com>
2019-03-22 14:32:00 +0100

    created an empty file for object detection

---
+++ object_detection.py
@@ -0,0 +1,8 @@
+import os, sys, time, datetime, random
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+from PIL import Image
+
